a8b10f4b5c6339f7d1bdacd8e65067269e9b09cf
Nasser Sanou <nassersanou23@gmail.com>
2018-08-07 03:17:40 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end 
 
-introduction("Nasser")
+introduction("Dan")
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{ name } and I am learning to program in #{ language }."
